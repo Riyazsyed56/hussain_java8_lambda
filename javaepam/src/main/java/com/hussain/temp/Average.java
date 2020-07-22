95feb8771eadcbf0e034c4c11c8ddb3b1306b855
riyaz <hussain>
2020-07-22 19:27:57 +0530

    commit

---
+++ javaepam/src/main/java/com/hussain/temp/Average.java
@@ -0,0 +1,10 @@
+package com.hussain.temp;
+
+
+import java.util.*;
+public class Average {
+	public double avg(List<Integer> list)
+	{
+		return list.stream().mapToInt(number -> number.intValue()).average().getAsDouble();
+	}
+}
